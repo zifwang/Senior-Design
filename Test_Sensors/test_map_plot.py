a3d52e9284ab427abb012523b50e1134e3a2a9bf
zifwang <zifwang@mymail.mines.edu>
2018-03-13 22:16:44 -0600

    plot works

---
+++ Test_Sensors/test_map_plot.py
@@ -0,0 +1,20 @@
+# Test map & plot function in python
+# import numpy as np                      # Enable numpy array
+# import matplotlib.pyplot as plt         # Enable plot function
+
+# # Set two variables
+# t = np.arange(0.0, 2.0, 0.01)           # t
+# s = np.sin(2*np.pi*t)                   # s
+
+# # Plot function
+# plt.plot(t,s)
+
+# # Setting title 
+# plt.title(r'$\alpha_i > \beta_i$', fontsize=20)
+
+# # Set x and y labels
+# plt.xlabel('time (s)')
+# plt.ylabel('volts (mV)')
+
+# # Show plot
+# plt.show()
